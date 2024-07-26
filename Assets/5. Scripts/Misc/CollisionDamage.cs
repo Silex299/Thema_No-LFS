dd1364e871d833f2319d666f0d3a33a591107070
Silex299 <ankurjyotinath99@gmail.com>
2024-07-26 21:11:54 +0530

    safe commit

--- Assets/5. Scripts/Misc/CollisionDamage.cs
+++ Assets/5. Scripts/Misc/CollisionDamage.cs
@@ -12,6 +12,7 @@ namespace Misc
         {
 
             if (!enabled) return;
+            print(collision.gameObject.name);
             if (collision.gameObject.CompareTag("Player_Main") || collision.gameObject.CompareTag("Player"))
             {
                 Player_Scripts.PlayerMovementController.Instance.player.Health.TakeDamage(maximumDamage);
