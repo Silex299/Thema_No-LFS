0f0bd189c6337ea8ee61324641439d6ada3deaae
Silex299 <ankurjyotinath99@gmail.com>
2024-07-27 17:39:35 +0530

    safe commit

--- Assets/5. Scripts/Triggers/WeightPressureTrigger.cs
+++ Assets/5. Scripts/Triggers/WeightPressureTrigger.cs
@@ -1,61 +1,91 @@
-using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class WeightPressureTrigger : MonoBehaviour
+namespace Triggers
 {
+    public class WeightPressureTrigger : MonoBehaviour
+    {
     
-    [SerializeField] public float requiredMass = 50f; // Set the required mass to activate the trigger
-    [SerializeField] private TMPro.TextMeshProUGUI pressureWeightText;
+        [SerializeField, BoxGroup("Properties")] public float requiredMass = 50f; // Set the required mass to activate the trigger
+        [SerializeField, BoxGroup("Properties")] private TMPro.TextMeshProUGUI pressureWeightText;
+
+        [BoxGroup("Events")] public UnityEvent activeTrigger;
+        [BoxGroup("Events")] public UnityEvent deActiveTrigger;
     
-    private float _currentMass = 0f;
-    private bool _isActivated = false;
+        private float _currentMass = 0f;
+        private bool _triggered;
+    
+    
 
     
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Interactable"))
+        private void OnTriggerEnter(Collider other)
         {
-            Rigidbody rb = other.attachedRigidbody;
-            _currentMass += rb.mass;
-            _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
-            UpdateWeightVisual();
+            if (other.CompareTag("Interactable"))
+            {
+                Rigidbody rb = other.attachedRigidbody;
+                _currentMass += rb.mass;
+                _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
+                UpdateWeightVisual();
+                CheckPressure();
+            }
+            else if (other.CompareTag("Player_Main"))
+            {
+                _currentMass += 25f;
+                _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
+                UpdateWeightVisual();
+                CheckPressure();
+            }
         }
-        else if (other.CompareTag("Player_Main"))
-        {
-            _currentMass += 25f;
-            _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
-            UpdateWeightVisual();
-        }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Interactable"))
+        private void OnTriggerExit(Collider other)
         {
-            Rigidbody rb = other.attachedRigidbody;
-            _currentMass -= rb.mass;
-            _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
-            UpdateWeightVisual();
+            if (other.CompareTag("Interactable"))
+            {
+                Rigidbody rb = other.attachedRigidbody;
+                _currentMass -= rb.mass;
+                _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
+                UpdateWeightVisual();
+                CheckPressure();
+            }
+            else if (other.CompareTag("Player_Main"))
+            {
+                _currentMass -= 25f;
+                _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
+                UpdateWeightVisual();
+                CheckPressure();
+            }
         }
-        else if (other.CompareTag("Player_Main"))
-        {
-            _currentMass -= 25f;
-            _currentMass = Mathf.Clamp(_currentMass, 0, Mathf.Infinity);
-            UpdateWeightVisual();
-        }
-    }
 
-    private void UpdateWeightVisual()
-    {
-        if (_currentMass == 0)
+
+
+        private void CheckPressure()
         {
-            pressureWeightText.text =  "00\n-\n" + requiredMass;
+            if (!_triggered && _currentMass >= requiredMass)
+            {
+                activeTrigger.Invoke();
+                _triggered = true;
+            }
+            else if(_triggered && _currentMass < requiredMass)
+            {
+                deActiveTrigger.Invoke();
+                _triggered = false;
+            }
         }
-        else
+    
+    
+        private void UpdateWeightVisual()
         {
-            pressureWeightText.text = Mathf.Clamp(_currentMass, 0, requiredMass) + "\n-\n" + requiredMass;
-        }
+            if (_currentMass == 0)
+            {
+                pressureWeightText.text =  "00\n-\n" + requiredMass;
+            }
+            else
+            {
+                pressureWeightText.text = Mathf.Clamp(_currentMass, 0, requiredMass) + "\n-\n" + requiredMass;
+            }
         
-    }
+        }
     
+    }
 }
