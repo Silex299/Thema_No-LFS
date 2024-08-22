27abde7419c1f2d49678a5e6a7e923f81785d237
Silex299 <ankurjyotinath99@gmail.com>
2024-08-22 15:54:40 +0530

    half water effects

--- Assets/5. Scripts/Player_Scripts/Volumes/HalfWaterVolume.cs
+++ Assets/5. Scripts/Player_Scripts/Volumes/HalfWaterVolume.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Player_Scripts.Volumes
 {
     public class HalfWaterVolume : MonoBehaviour
     {
+        public bool continuousCheck;
+        
         public GameObject splashEffect;
         public GameObject dragEffect;
 
@@ -16,20 +20,15 @@ namespace Player_Scripts.Volumes
         
         private void OnTriggerEnter(Collider other)
         {
+            if(continuousCheck)return;
             if (other.CompareTag("Player_Main"))
             {
-                var playerVelocity = other.GetComponent<PlayerVelocityCalculator>();
-                if(playerVelocity.velocity.y < -velocityThreshold)
-                {
-                    Instantiate(splashEffect, other.transform.position, Quaternion.identity, transform);
-                }
-                _spawnedDragEffect ??= Instantiate(dragEffect);
-                _playerTransform = other.transform;
+                SpawnEffects(other.transform);
             }
         }
-
         private void OnTriggerExit(Collider other)
         {
+            if(continuousCheck)return;
             if (other.CompareTag("Player_Main"))
             {
                 Destroy(_spawnedDragEffect);
@@ -38,16 +37,68 @@ namespace Player_Scripts.Volumes
             }
         }
 
+        #region Contiunous Check
+        
+        private Coroutine _resetTrigger;
+        private void OnTriggerStay(Collider other)
+        {
+            if (!continuousCheck) return;
+            
+            if (other.CompareTag("Player_Main"))
+            {
+                if (_resetTrigger == null)
+                {
+                    SpawnEffects(other.transform);
+                }
+                else
+                {
+                    StopCoroutine(_resetTrigger);
+                }
+                _resetTrigger = StartCoroutine(ResetPlayerTrigger());
+            }
+        }
+        IEnumerator ResetPlayerTrigger()
+        {
+            yield return new WaitForSeconds(0.5f);
+            Destroy(_spawnedDragEffect);
+            _spawnedDragEffect = null;
+            _playerTransform = null;
+            _resetTrigger = null;
+        }
+
+        #endregion
+        
+        
+
         private void Update()
         {
             if (_spawnedDragEffect && _playerTransform)
             {
                 var dragLocalPos = transform.InverseTransformPoint(_playerTransform.position);
                 dragLocalPos.y = dragRestrictedY;
-                dragLocalPos += dragEffectOffset;
+                dragLocalPos += _playerTransform.TransformDirection(dragEffectOffset);
                 _spawnedDragEffect.transform.localPosition = dragLocalPos;
                 
             }
         }
+
+        private void SpawnEffects(Transform other)
+        {
+            
+            var playerVelocity = other.GetComponent<PlayerVelocityCalculator>();
+            
+            print(playerVelocity.velocity.y);
+            
+            if(playerVelocity.velocity.y < -velocityThreshold)
+            {
+                var desiredPos = other.transform.position;
+                desiredPos.y = (transform.transform.up * dragRestrictedY).y;
+                Instantiate(splashEffect, desiredPos, Quaternion.identity);
+                
+            }
+            _spawnedDragEffect ??= Instantiate(dragEffect, parent: transform);
+            _playerTransform = other;
+        }
+        
     }
 }
