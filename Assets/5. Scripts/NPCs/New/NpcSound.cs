07f5e637003a3ac7cf84055852bb43b86818a9be
Silex299 <ankurjyotinath99@gmail.com>
2024-09-15 04:43:44 +0530

    guard step soound

---
+++ Assets/5. Scripts/NPCs/New/NpcSound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace NPCs.New
+{
+    
+    /**
+     * This script doesn't contain any advanced logic for surface detection or footstep effect
+     **/
+    public class NpcSound : MonoBehaviour
+    {
+        
+        public AudioSource audioSource;
+        public float volumeMultiplier;
+        public AudioClip[] soundClips;
+        public AudioClip[] otherSoundClips;
+        
+        public void PlayFootstepSound()
+        {
+            audioSource.PlayOneShot(soundClips[Random.Range(0, soundClips.Length)], volumeMultiplier);
+        }
+
+        public void PlayOtherSound(int index)
+        {
+            audioSource.PlayOneShot(otherSoundClips[index], volumeMultiplier);
+        }
+        
+    }
+}
---
+++ Assets/5. Scripts/Scene Scripts/Experiments.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using Managers;
+using Managers.Checkpoints;
+using UnityEngine;
+
+namespace Scene_Scripts
+{
+    public class Experiments : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            CheckpointManager.Instance.onCheckpointLoad += OnCheckpointLoad;
+        }
+
+        private void OnDisable()
+        {
+            CheckpointManager.Instance.onCheckpointLoad -= OnCheckpointLoad;
+        }
+        
+        private void OnCheckpointLoad(int checkpoint)
+        {
+            if (checkpoint > 0)
+            {
+                StartCoroutine(UiUpdateOnCheckpointLoad());
+            }
+        }
+
+        private IEnumerator UiUpdateOnCheckpointLoad()
+        {
+            yield return new WaitForSeconds(1f);
+            UIManager.Instance.FadeOut(1f);
+        }
+        
+    }
+}
