63818dd2d421350e1b7c2b3700404fe894162a5e
Silex299 <ankurjyotinath99@gmail.com>
2024-09-25 05:01:58 +0530

    safe commit

--- Assets/5. Scripts/Misc/ChangeMaterials.cs
+++ Assets/5. Scripts/Misc/ChangeMaterials.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Misc
@@ -8,24 +9,31 @@ namespace Misc
         [SerializeField] private Material defaultMaterial;
         [SerializeField] private Material highlightMaterial;
         [SerializeField] private int materialIndex;
-    
+
+
+        private Renderer _renderer;
+
+        private void Start()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
+
+
         //Create a function with a bool parameter to change the material with index materialIndex to the defaultMaterial or highlightMaterial based on the bool parameter value
         public void ChangeMaterial(bool isHighlighted)
         {
-            //Get the renderer component of the object
-            var renderer = GetComponent<Renderer>();
             //Check if the renderer is not null
-            if (renderer == null) return;
+            if (!_renderer) return;
         
             //Get the materials array from the renderer
-            var materials = renderer.materials;
+            var materials = _renderer.materials;
             //Check if the materialIndex is within the bounds of the materials array
             if (materialIndex >= 0 && materialIndex < materials.Length)
             {
                 //Set the material at the materialIndex to the defaultMaterial or highlightMaterial based on the isHighlighted parameter
                 materials[materialIndex] = isHighlighted ? highlightMaterial : defaultMaterial;
                 //Assign the modified materials array back to the renderer
-                renderer.materials = materials;
+                _renderer.materials = materials;
             }
         }
     
---
+++ Assets/5. Scripts/Misc/SightDetectionVisualExtension.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Misc
+{
+    public class SightDetectionVisualExtension : MonoBehaviour
+    {
+        
+        [FoldoutGroup("Visual")] public GameObject visualElement;
+        [Required] [FoldoutGroup("Visual")] public ChangeMaterials materialChanger;
+        
+        [FoldoutGroup("Flicker")] public int flickerCount = 1;
+        [FoldoutGroup("Flicker")] public float flickerInterval = 0.4f;
+        
+        [FoldoutGroup("Sound")] public AudioSource audioSource;
+        [FoldoutGroup("Sound")] public AudioClip detectionSound;
+
+
+        private Coroutine _detectionCoroutine;
+
+        [Button("Test Flicker")]
+        public void OnDetection()
+        {
+            _detectionCoroutine ??= StartCoroutine(DetectionFlicker());
+            audioSource?.PlayOneShot(detectionSound);
+        }
+
+        private IEnumerator DetectionFlicker()
+        {
+            for (int i = 0; i < flickerCount; i++)
+            {
+                visualElement.SetActive(false);
+                materialChanger.ChangeMaterial(true);
+                yield return new WaitForSeconds(flickerInterval);
+                visualElement.SetActive(true);
+                materialChanger.ChangeMaterial(false);
+                yield return new WaitForSeconds(flickerInterval);
+            }
+            
+            _detectionCoroutine = null;
+        }
+    }
+}
