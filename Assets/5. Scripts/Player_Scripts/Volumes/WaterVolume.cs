2928a6d5cc95f4dc09dc7cbf2409f5e69ae9c67a
Silex299 <ankurjyotinath99@gmail.com>
2024-10-16 23:59:39 +0530

    Progress commit - Water effects updated

--- Assets/5. Scripts/Player_Scripts/Volumes/WaterVolume.cs
+++ Assets/5. Scripts/Player_Scripts/Volumes/WaterVolume.cs
@@ -13,6 +13,16 @@ namespace Player_Scripts.Volumes
         [FoldoutGroup("Volume Property"), Tooltip("Defines after what y position the player state is changed to water")] 
         public float triggerYThreshold;
         [FoldoutGroup("Volume Property")] public float damageSpeed;
+        
+        [FoldoutGroup("Particle Effects")] public GameObject splashEffect;
+        [FoldoutGroup("Particle Effects")] public Vector3 splashEffectOffset;
+        [FoldoutGroup("Particle Effects"), Space(10)]
+        public GameObject dragEffect;
+        [FoldoutGroup("Particle Effects")] public Vector3 dragEffectOffset;
+        [FoldoutGroup("Particle Effects"), Space(10)]
+        public GameObject underwaterEffect;
+        [FoldoutGroup("Particle Effects"), Space(10)]
+        public Vector3 underwaterEffectOffset;
 
         [FoldoutGroup("Camera Offsets")] public ChangeOffset underwaterCameraOffset;
         [FoldoutGroup("Camera Offsets")] public ChangeOffset aboveWaterCameraOffset;
@@ -21,8 +31,18 @@ namespace Player_Scripts.Volumes
         private Coroutine _triggerCoroutine;
         private bool _triggered;
         private bool _playerOnSurface;
+        private GameObject _spawnedDragEffect;
+        private GameObject _spawnedUnderwaterEffect;
+        
         private Player Player => PlayerMovementController.Instance.player;
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!enabled) return;
+            if (!other.CompareTag("Player_Main")) return;
+            SpawnSplash();
+        }
+        
         private void OnTriggerStay(Collider other)
         {
             if (!enabled) return;
@@ -40,22 +60,27 @@ namespace Player_Scripts.Volumes
         {
             yield return new WaitForSeconds(0.2f);
             
+            DestroySpawnedEffects();
             Physics.gravity = new Vector3(0, -9.81f, 0);
             _triggered = false;
             _triggerCoroutine = null;
+            
         }
         private void TriggerWaterVolume()
         {
             if (!enabled) return;
+            
             if(triggerYThreshold < Player.transform.position.y) return;
             
             _triggered = true;
-
+            SpawnEffects(false);
+        
             Physics.gravity = new Vector3(0, -0.5f, 0);
             if (PlayerMovementController.Instance.player.currentStateIndex != 2)
             {
                 PlayerMovementController.Instance.ChangeState(2);
             }
+            
         }
         private void OnDrawGizmos()
         {
@@ -76,20 +101,22 @@ namespace Player_Scripts.Volumes
         {
             if (!_triggered) return;
 
+           
             bool onSurface = (Player.transform.position.y + 1.5f) >= surfaceLevel;
-            if (onSurface != _playerOnSurface)
-            {
-                Player.waterMovement.OnSurface = onSurface;
-                _playerOnSurface = onSurface;
+            if (onSurface == _playerOnSurface) return;
+            
+            
+            Player.waterMovement.OnSurface = onSurface;
+            _playerOnSurface = onSurface;
+            SpawnEffects(onSurface);
                 
-                if (_playerOnSurface)
-                {
-                    aboveWaterCameraOffset.ChangeCameraOffset();
-                }
-                else
-                {
-                    underwaterCameraOffset.ChangeCameraOffset();
-                }
+            if (_playerOnSurface)
+            {
+                aboveWaterCameraOffset.ChangeCameraOffset();
+            }
+            else
+            {
+                underwaterCameraOffset.ChangeCameraOffset();
             }
         }
 
@@ -116,5 +143,51 @@ namespace Player_Scripts.Volumes
 
             #endregion
         }
+
+
+        #region Particle
+
+        private void SpawnSplash()
+        {
+            if(triggerYThreshold > Player.transform.position.y) return;
+            Instantiate(splashEffect, Player.transform.position + splashEffectOffset, Quaternion.identity);
+        }
+
+        private void SpawnEffects(bool onSurface)
+        {
+            if (!_spawnedDragEffect)
+            {
+                _spawnedDragEffect = Instantiate(dragEffect, parent: Player.transform);
+                _spawnedDragEffect.transform.localPosition = dragEffectOffset;
+                _spawnedDragEffect.transform.localRotation = Quaternion.identity;
+            }
+
+            if (!_spawnedUnderwaterEffect)
+            {
+                _spawnedUnderwaterEffect = Instantiate(underwaterEffect, parent: Player.transform);
+                _spawnedUnderwaterEffect.transform.localPosition = underwaterEffectOffset;
+                _spawnedUnderwaterEffect.transform.localRotation = Quaternion.identity;
+            }
+            
+            _spawnedDragEffect.SetActive(onSurface);
+            _spawnedUnderwaterEffect.SetActive(!onSurface);
+            
+        }
+        private void DestroySpawnedEffects()
+        {
+            if (_spawnedDragEffect)
+            {
+                Destroy(_spawnedDragEffect);
+                _spawnedDragEffect = null;
+            }
+            if (_spawnedUnderwaterEffect)
+            {
+                Destroy(_spawnedUnderwaterEffect);
+                _spawnedUnderwaterEffect = null;
+            }
+        }
+
+        #endregion
+        
     }
 }
